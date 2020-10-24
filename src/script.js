8c1b025424aa56b1f3e2c23c24793d8552e25ba1
maystar236 <70463399+maystar236@users.noreply.github.com>
2020-10-24 19:43:29 +1100

    Fixed padding on forecast

--- src/script.js
+++ src/script.js
@@ -141,7 +141,7 @@ function displayForecast(response) {
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
-    <div class="col">
+    <div class="col-2">
       <h3>
         ${formatHours(forecast.dt * 1000)}
       </h3>
